31e4f0e30ff89a332948309ad6742988a755a278
Kristian A Corrales <102649276+Kristian-Corrales@users.noreply.github.com>
2022-07-29 20:27:27 +0800

    Delete HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		//This is to print output.
-		System.out.println("Hello to all! Kamusta kayo? Hola! Bonjour! guten tag! salve! nin hao! ola! konnichiwa!");
-	}	
-}
