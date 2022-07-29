bf0be8c87446ebe1c9642c5658b8101c6376e218
Kristian Corrales <kristian.corrales.osv@fedex.com>
2022-07-29 15:14:48 +0800

    [JavaTcoding]First commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,7 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//This is to print output.
+		System.out.println("Hello to all! Kamusta kayo? Hola! Bonjour! guten tag! salve! nin hao! ola! konnichiwa!");
+	}	
+}
